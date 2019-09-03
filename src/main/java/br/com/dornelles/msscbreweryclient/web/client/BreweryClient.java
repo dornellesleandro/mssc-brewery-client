69e4e3405c0f818e546fb4ea00bf3d4e63b92d48
leandrosdornelles@gmail.com <lsd09491090>
2019-09-03 05:00:42 -0300

    Created call to save new beer. close #3

--- src/main/java/br/com/dornelles/msscbreweryclient/web/client/BreweryClient.java
+++ src/main/java/br/com/dornelles/msscbreweryclient/web/client/BreweryClient.java
@@ -6,6 +6,7 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import java.net.URI;
 import java.util.UUID;
 
 @Component
@@ -26,5 +27,9 @@ public class BreweryClient {
 
     public BeerDto getBeerById(UUID id) {
         return restTemplate.getForObject(apiHost + BEER_PATH_V2 + id.toString(), BeerDto.class);
+    }
+
+    public URI saveNewBeer(BeerDto beerDto) {
+        return restTemplate.postForLocation(apiHost + BEER_PATH_V2, beerDto);
     }
 }
